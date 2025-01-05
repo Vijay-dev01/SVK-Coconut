2d1dfabaad6b339616eeb7980c0530f4665ae713
Vijay-dev01 <vijayajay3535@gmail.com>
2025-01-05 20:39:30 +0530

    Refactor header section to include background video and adjust overlay styles

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,22 +4,28 @@ export const Header = (props) => {
   return (
     <header id="header">
       <div className="intro">
-        <div className="overlay">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1>
-                  {props.data ? props.data.title : "Loading"}
-                  <span></span>
-                </h1>
-                <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                <a
-                  href="#features"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Learn More
-                </a>{" "}
-              </div>
+        <div className="overlay"></div>
+        <iframe
+          src="https://www.youtube.com/embed/hIjzJ-vRRCQ?autoplay=1&mute=1&loop=1&playlist=hIjzJ-vRRCQ"
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+          title="SVKCoconut Background Video"
+        ></iframe>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 col-md-offset-2 intro-text">
+              <h1>
+                {props.data ? props.data.title : "Loading"}
+                <span></span>
+              </h1>
+              <p>{props.data ? props.data.paragraph : "Loading"}</p>
+              <a
+                href="#features"
+                className="btn btn-custom btn-lg page-scroll"
+              >
+                Learn More
+              </a>
             </div>
           </div>
         </div>
