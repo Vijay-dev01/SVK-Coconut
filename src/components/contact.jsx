71d345a687fb658dacf417f167ad75f7f5a9f5ed
Vijay-dev01 <vijayajay3535@gmail.com>
2025-01-09 15:52:58 +0530

    Update color scheme in CSS and modify footer credits in Contact component

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -197,13 +197,16 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
+            &copy; Crafted with passion in 2025. Designed and built by{" "}
+            <a href="https://vijayg-portfolio-web.netlify.app/" rel="nofollow">
+              Vijay G
             </a>
+            . Thank you for visiting our website and supporting our journey. We
+            strive to innovate and bring meaningful solutions to the world.
           </p>
         </div>
       </div>
+
     </div>
   );
 };
