94cd73905cfc1e4bdc5792437e29bb1570c666a8
Vijay-dev01 <vijayajay3535@gmail.com>
2025-01-16 21:15:03 +0530

    gallery des added.

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -8,25 +8,24 @@ export const Gallery = (props) => {
         <div className="section-title">
           <h2>Gallery</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            Explore the beauty of our coconut plantations, showcasing the lush greenery and the finest coconuts cultivated with care.
           </p>
         </div>
         <div className="row">
           <div className="portfolio-items">
             {props.data
               ? props.data.map((d, i) => (
-                  <div
-                    key={`${d.title}-${i}`}
-                    className="col-sm-6 col-md-4 col-lg-4"
-                  >
-                    <Image
-                      title={d.title}
-                      largeImage={d.largeImage}
-                      smallImage={d.smallImage}
-                    />
-                  </div>
-                ))
+                <div
+                  key={`${d.title}-${i}`}
+                  className="col-sm-6 col-md-4 col-lg-4"
+                >
+                  <Image
+                    title={d.title}
+                    largeImage={d.largeImage}
+                    smallImage={d.smallImage}
+                  />
+                </div>
+              ))
               : "Loading..."}
           </div>
         </div>
