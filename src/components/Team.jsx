d6fb8a1da31ce7efc87ee9a758277a6885bc5e43
Vijay-dev01 <vijayajay3535@gmail.com>
2025-01-18 23:11:21 +0530

    Added team mate description.

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -7,8 +7,7 @@ export const Team = (props) => {
         <div className="col-md-8 col-md-offset-2 section-title">
           <h2>Meet the Team</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+           Our passionate team of coconut enthusiasts at dedicated to delivering the finest, freshest coconuts and innovation products with care and expertise.
           </p>
         </div>
         <div id="row">
